2c6bec10af8f75f163c8eeb92dea2cd29d4b8a14
Ciibelle <cibellegon6565@gmail.com>
2022-05-24 21:04:13 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorVendido = 500000;
+            int porcentagem = 5;
+            int resultado = 0;
+
+            resultado = (valorVendido * porcentagem)/100;
+            Console.WriteLine($"Os 5% de 500000 é: {resultado}");
+        }
+    }
+}
